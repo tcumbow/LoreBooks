07ad728ed0229dc97f84ff705d1036c78a37c4c8
Tom Cumbow <tcumbow@icloud.com>
2020-12-02 13:34:36 -0500

    sync script

--- Sync.ps1
+++ Sync.ps1
@@ -1,3 +1,3 @@
 unison .\LoreBooks A:\LoreBooks -auto -batch
-#unison .\LoreBooks R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\LoreBooks -auto -batch
-#unison .\LoreBooks "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\LoreBooks" -auto -batch
+unison .\LoreBooks R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\LoreBooks -auto -batch
+unison .\LoreBooks "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\LoreBooks" -auto -batch
