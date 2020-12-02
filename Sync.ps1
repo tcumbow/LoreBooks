107ff913eb09911c11709bdbeb75e3c69c9aca08
Tom Cumbow <tcumbow@icloud.com>
2020-12-02 15:25:32 -0500

    added pauses

--- Sync.ps1
+++ Sync.ps1
@@ -1,3 +1,5 @@
 unison .\LoreBooks A:\LoreBooks -auto -batch
+pause
 unison .\LoreBooks R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\LoreBooks -auto -batch
+pause
 unison .\LoreBooks "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\LoreBooks" -auto -batch
