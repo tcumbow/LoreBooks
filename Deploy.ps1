cdafa2b945e3698f071d584754a95478d9b8d1d9
Tom Cumbow <tcumbow@icloud.com>
2020-12-29 18:52:35 -0500

    new deploy model

---
+++ Deploy.ps1
@@ -0,0 +1,2 @@
+robocopy .\LoreBooks R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\LoreBooks /mir
+robocopy .\LoreBooks "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\LoreBooks" /mir
--- Sync.ps1
+++
@@ -1,5 +0,0 @@
-unison .\LoreBooks A:\LoreBooks -auto -batch
-pause
-unison .\LoreBooks R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\LoreBooks -auto -batch
-pause
-unison .\LoreBooks "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\LoreBooks" -auto -batch
