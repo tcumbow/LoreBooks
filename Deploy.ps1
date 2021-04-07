d66a334677796e9d86effb8825341cdf3672fc89
Tom Cumbow <tcumbow@icloud.com>
2021-04-07 19:43:19 -0400

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,2 +1,2 @@
-robocopy .\LoreBooks R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\LoreBooks /mir
-robocopy .\LoreBooks "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\LoreBooks" /mir
+robocopy .\LoreBooks J:\LoreBooks /mir
+robocopy .\LoreBooks S:\LoreBooks /mir
